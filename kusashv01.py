77ee87c1793c28bda6e2b456f491cb90cb2fa00e
kusapi <otistikadam99b@gmail.com>
2026-04-07 19:25:56 +0300

    Add files via upload

--- kusashv01.py
+++ kusashv01.py
@@ -1,132 +1,63 @@
-from os import system, chdir, getcwd
+from sys import platform
+import platform as pf
 from rich import print
 from rich.console import Console
 from rich.panel import Panel
-from rich.prompt import Prompt
-from pyfiglet import figlet_format as figlet
+from pyfiglet import figlet_format
 import getpass
+from os import system
+helplist="""Ekler kullanımı: 
+    -p -->  try -p <Yazı>
+        çıktı: Yazı
+    -e --> try -e <Your Terminal Code here>
+        örnek kullanım! --> try -e echo Hello World!
+            çıktı: Hello World!
+    -pkg --> try -pkg install <paket adı>
+         --> try -pkg remove <paket adı>
+         --> try -pkg list <paket adı>
+        !NOT! --> Bu pkg eki apt paket yöneticisi kullanır eğer apt paket yöneticiniz yok ise hata alırsınız!!!
+    --kusafetch --> try --kusafetch
+        çıktı: <sistem bilgileriniz>
+    ! SÜRÜM --> v0.1 !"""
 
-console=Console()
-username=getpass.getuser()
-header=figlet("KuSaSH\nCustom Shell")
-path=getcwd()
-
-def Value_Error_Message(value):
-    print(f"[red]!HATA![red]: Yanlış değer girdiniz! --> '{value}'")
-
-def FileNotFound_Error_Message(path):
-    print(f"[red]!HATA![/red]: Yanlış veya Bilinmeyen Konum! --> '{path}'")
-
-def Index_Error_Message(komut):
-    print(f"[red]!HATA![/red]: Yanlış, Eksik veya Bilinmeyen komut --> '{komut}'\n Yardım için --> 'try --help' dene!")
-
-def exit_the_shell():
-    system("clear")
-    print("![red]Çıkış[/red] [green]Başarılı[/green] | [red]Exit[/red] is [green]Succesful[/green]!")
-    exit()
+console = Console()
 system("clear")
-console.print(Panel(f"[red]{header}[/red]\n[yellow]yardım için 'try --help' yazın![/yellow]"))
+h = figlet_format("KusaShell", font="slant")
+console.print(Panel(f"{h}\nYardim icin ---> 'try --help'"))
+uname = pf.uname()
 
-try:
-    while True:
-        command_prompt=Prompt.ask(f"[green]{username}[/green] ~ [green]{path}[/green] -->    ")
-        command_parts=command_prompt.split()
-        try:
-            match command_parts[0]:
-                case "exit":
-                    exit_the_shell()
-                case "network":
-                    try:
-                        match command_parts[1]:
-                            case "-mgr":
-                                system("iwctl")
-                            case "--ping":
-                                target_ip = Prompt.ask("[yellow]IP address[/yellow]").strip()
-                                if target_ip == "":
-                                    Value_Error_Message(target_ip)
-                                    continue  # shell loop devam eder
-
-                                ping_count_str = Prompt.ask("[yellow]Ping Count[/yellow]").strip()
-                                if ping_count_str == "":
-                                    Value_Error_Message(ping_count_str)
-                                    continue
-
-                                try:
-                                    ping_count = int(ping_count_str)
-                                except ValueError:
-                                    Value_Error_Message(ping_count_str)
-                                    continue
-
-                                system(f"ping -c {ping_count} {target_ip}")
-
-                            case "--scan":
-                                system("nmcli dev wifi rescan && nmcli -f IN-USE,SSID,SIGNAL dev wifi list")
-
-                            case "--nmap":
-                                nmap_command=Prompt.ask("[yellow]Nmap Komutu(başa nmap koymayın)    [/yellow]")
-                                system(f"sudo nmap {nmap_command}")
-
-                    except IndexError:
-                        Index_Error_Message(command_prompt)
-                case "try":
-                    try:
-                        match command_parts[1]:
-                            case "--help":
-                                system("cat help.txt")
-                            case "-p":
-                                try:
-                                    try:
-                                        yazi=" ".join(command_parts[2:])
-                                        print(f"[yellow]{yazi}[/yellow]")
-                                    except IndexError:
-                                        Index_Error_Message(command_prompt)
-
-                                except IndexError:
-                                    Index_Error_Message(command_prompt)
-                            case "-e":
-                                try:
-                                    exect = " ".join(command_parts[2:])
-                                    if(exect==""):
-                                        print("[red]!HATA![/red] exec(-e) ekinden sonra bir veya birden fazla sistem kodu gelmeli!")
-                                    else:
-                                        system(exect)
-                                except IndexError:
-                                    Index_Error_Message(command_prompt)
-                            case "--e":
-                                try:
-                                    exect = " ".join(command_parts[2:])
-                                    if(exect!=""):
-                                        system(f"sudo {exect}")
-                                    else:
-                                        print("[red]!HATA![/red] exec(-e) ekinden sonra bir veya birden fazla sistem kodu gelmeli!")
-                                except IndexError:
-                                    Index_Error_Message(command_prompt)
-                            case "--kusafetch":
-                                exect="neofetch"
-                                system(exect)
-                            case "-cd":
-                                try:
-                                    try:
-                                        go_to_path=" ".join(command_parts[2:])
-                                        chdir(go_to_path)
-                                    except FileNotFoundError:
-                                        FileNotFound_Error_Message(go_to_path)
-
-                                except IndexError:
-                                    Index_Error_Message(command_prompt)
-                            case "-pkg":
-                                package_manager=Prompt.ask("[yellow]Paket Yöneticinizin adı [/yellow]")
-                                package_name=Prompt.ask("[yellow]Indirmek istediğiniiz paketin adı  [/yellow]")
-                                system(f"sudo {package_manager} install {package_name}")
-                            case _:
-                                Index_Error_Message(command_prompt)
-
-                    except IndexError:
-                        Index_Error_Message(command_prompt)
-                case _:
-                    Index_Error_Message(command_prompt)
-        except IndexError:
-            Index_Error_Message(command_prompt)
-
-except (KeyboardInterrupt, EOFError):
-    exit_the_shell()
+while True:
+    username = getpass.getuser()
+    cmd = input(f"{username} | KuSaShell ~$   ")
+    cmd = cmd.split()
+    if(cmd[0] != "try"):
+        non=cmd[0]
+        print(f"[red]!HATA! --> Bilinmeyen[/red][cyan] '{non}'[/cyan], Yardım için: 'try --help'")
+    else:
+        if(cmd[1] == "-p"):
+            yazi = " ".join(cmd[2:])
+            print(f"[yellow]{yazi}[/yellow]")
+        elif(cmd[1] == "-e"):
+            exec = " ".join(cmd[2:])
+            system(exec)
+        elif(cmd[1] == "-pkg"):
+            if(cmd[2] == "install"):
+                pack = " ".join(cmd[3:])
+                system(f"apt install {pack} -y")
+            elif(cmd[2] == "remove"):
+                pack = " ".join(cmd[3:])
+                system(f"apt remove {pack} -y")
+            elif(cmd[2] == "list"):
+                system("apt list --installed")
+            else:
+                ek = cmd[2]
+                print(f"[red]!HATA! --> Bilinmeyen Ek[/red][cyan] '{ek}'[/cyan], Yardım için 'try --help'")
+        elif(cmd[1] == "--kusafetch"):
+            print(uname)
+        elif(cmd[1] == "--help"):
+            print(helplist)
+        elif(cmd[1] == "--whoami"):
+            print(username)
+        else:
+            ek = cmd[1]
+            print(f"[red]!HATA! --> Bilinmeyen Ek[/red][cyan] '{ek}'[/cyan], Yardım için 'try --help'")
